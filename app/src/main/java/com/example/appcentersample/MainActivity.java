4f8a394d47541676cb569eaad820431208d98162
Christopher Rodriguez <christophererodriguez70@gmail.com>
2020-05-01 10:24:19 -0500

    Updated version code

--- app/src/main/java/com/example/appcentersample/MainActivity.java
+++ app/src/main/java/com/example/appcentersample/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.appcentersample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         AppCenter.start(getApplication(), "da13410d-be8a-4b3e-bf2a-8cae42ed0031",
                 Analytics.class, Crashes.class);
+
+
+        button1 = (Button) findViewById(R.id.button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "The button was clicked", Toast.LENGTH_LONG).show();
+                Crashes.generateTestCrash();
+            }
+        });
     }
 }
